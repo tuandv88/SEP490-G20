6030dce9b190783c5903a6cd75b17bc2ca0e7de4
truongBui2002 <buivantruong16082002@gmail.com>
2024-10-21 21:59:20 +0700

    delete client test identity

--- backend/MoviesClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MoviesClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

e5f13dd69480b14ecfac100d52c718cd3d2bdd06
hamanhfu.me <hamanhfu.me@gmail.com>
2024-11-11 15:21:44 +0700

    create api userdiscussion

---
+++ backend/Services/Community/Community.API/Endpoints/UserDiscussions/UpdateUserDiscussionEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Community.Application.Models.UserDiscussions.Commands.UpdateUserDiscussion;
+using Community.Application.Models.UserDiscussions.Dtos;
+
+namespace Community.API.Endpoints.UserDiscussions;
+
+public class UpdateUserDiscussionEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/userdiscussions", async (UpdateUserDiscussionDto updateUserDiscussionDto, ISender sender) =>
+        {
+            var command = new UpdateUserDiscussionCommand(updateUserDiscussionDto);
+            var result = await sender.Send(command);
+
+            if (result.IsSuccess)
+            {
+                return Results.Ok(new { Message = "UserDiscussion updated successfully", NewStatus = result.IsSuccess });
+            }
+            else
+            {
+                return Results.BadRequest(new { Message = "Failed to update UserDiscussion", NewStatus = result.IsSuccess });
+            }
+        })
+        .WithName("UpdateUserDiscussion")
+        .Produces(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Update a UserDiscussion by its ID.");
+    }
+}
--- backend/Services/Community/Community.Application/Models/Votes/Queries/GetVoteById/GetVoteByIdQuery.cs
+++ backend/Services/Community/Community.Application/Models/Votes/Queries/GetVoteById/GetVoteByIdQuery.cs
@@ -1,9 +1,7 @@
-﻿
-using Community.Application.Models.Votes.Dtos;
+﻿using Community.Application.Models.Votes.Dtos;
 
-namespace Community.Application.Models.Votes.Queries.GetVoteById
-{
-    public record GetVoteByIdResult(VoteDto VoteDto);
+namespace Community.Application.Models.Votes.Queries.GetVoteById;
 
-    public record GetVoteByIdQuery(Guid Id) : IQuery<GetVoteByIdResult>;
-}
+public record GetVoteByIdResult(VoteDto VoteDto);
+
+public record GetVoteByIdQuery(Guid Id) : IQuery<GetVoteByIdResult>;

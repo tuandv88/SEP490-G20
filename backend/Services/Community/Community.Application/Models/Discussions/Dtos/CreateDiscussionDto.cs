694717dac16858ff3eba26b09d1489618d638287
hamanhfu.me <hamanhfu.me@gmail.com>
2024-11-11 19:02:52 +0700

    create discussion api

---
+++ backend/Services/Community/Community.Application/Models/Discussions/Dtos/CreateDiscussionDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Community.Application.Models.Discussions.Dtos;
+
+public record CreateDiscussionDto(
+    Guid UserId,                           // ID của người tạo thảo luận
+    Guid CategoryId,                       // ID của chuyên mục
+    string Title,                          // Tiêu đề thảo luận
+    string Description,                    // Mô tả nội dung
+    List<string> Tags,                     // Danh sách tags
+    ImageDto? Image,                       // Thông tin ảnh (nếu có)
+    bool IsActive                          // Trạng thái hoạt động
+);
+
+public record ImageDto(
+    string FileName,                       // Tên file ảnh
+    string Base64Image,                    // Nội dung ảnh ở định dạng Base64
+    string ContentType                     // Loại nội dung của ảnh
+);
---
+++ backend/Services/Community/Community.Infrastructure/Services/Base64Converter.cs
@@ -0,0 +1,13 @@
+﻿using Community.Application.Interfaces;
+
+namespace Community.Infrastructure.Services;
+
+public class Base64Converter : IBase64Converter
+{
+    public MemoryStream ConvertToMemoryStream(string base64String)
+    {
+        var base64Data = base64String.Contains(",") ? base64String.Split(',')[1] : base64String;
+        byte[] imageBytes = Convert.FromBase64String(base64Data);
+        return new MemoryStream(imageBytes);
+    }
+}

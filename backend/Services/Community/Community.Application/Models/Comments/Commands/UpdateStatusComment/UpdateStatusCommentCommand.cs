2565c23bce4ca3bebd99395f66632c65c5ad9732
hamanhfu.me <hamanhfu.me@gmail.com>
2024-11-11 07:01:17 +0700

    Create API: Update Category & StatusCategoryById

--- backend/Services/Community/Community.Application/Models/Comments/Commands/UpdateStatusComment/UpdateStatusCommentCommand.cs
+++ backend/Services/Community/Community.Application/Models/Comments/Commands/UpdateStatusComment/UpdateStatusCommentCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Community.Application.Models.Comments.Commands.UpdateStatusComment;
+﻿namespace Community.Application.Models.Comments.Commands.UpdateStatusComment;
 
 public record UpdateStatusCommentResult(bool IsSuccess, bool NewStatus);
 public record UpdateStatusCommentCommand(Guid Id) : ICommand<UpdateStatusCommentResult>;

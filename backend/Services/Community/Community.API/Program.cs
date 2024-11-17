7f544641866d7764c2388c120b97c62a4dd992d6
hamanhfu.me <hamanhfu.me@gmail.com>
2024-11-17 23:23:49 +0700

    update configure log

--- backend/Services/Community/Community.API/Program.cs
+++ backend/Services/Community/Community.API/Program.cs
@@ -5,7 +5,23 @@ using BuildingBlocks.Logging;
 var builder = WebApplication.CreateBuilder(args);
 
 //Logging
-builder.Host.UseSerilog(SeriLogger.Configure);
+builder.Host.UseSerilog(SeriLogger.Configure)
+    .ConfigureServices(services => {
+    //Add Service
+    services
+        .AddApplicationServices(builder.Configuration)
+        .AddInfrastructureServices(builder.Configuration)
+        .AddApiServices(builder.Configuration);
+
+    //Docs API
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen(options => {
+        options.EnableAnnotations();
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "AI API", Version = "v1" });
+    });
+
+    services.AddControllers();
+});
 
 //Docs API
 builder.Services.AddEndpointsApiExplorer();

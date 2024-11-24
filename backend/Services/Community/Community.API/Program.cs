b8306e5084600e83e7e41a87b67c8c1807ac55ec
hamanhfu.me <hamanhfu.me@gmail.com>
2024-11-25 00:54:20 +0700

    fix

--- backend/Services/Community/Community.API/Program.cs
+++ backend/Services/Community/Community.API/Program.cs
@@ -21,19 +21,6 @@ builder.Host.UseSerilog(SeriLogger.Configure)
             options.EnableAnnotations();
             options.SwaggerDoc("v1", new OpenApiInfo { Title = "Discussion API", Version = "v1" });
         });
-
-        services.AddControllers();
-
-        // Thêm CORS
-        services.AddCors(options =>
-        {
-            options.AddPolicy("AllowReactApp", policy =>
-            {
-                policy.WithOrigins("http://localhost:5173")  // Địa chỉ của ứng dụng React
-                      .AllowAnyHeader()   // Cho phép bất kỳ header nào
-                      .AllowAnyMethod();  // Cho phép bất kỳ phương thức HTTP nào (GET, POST, PUT, DELETE...)
-            });
-        });
     });
 
 var app = builder.Build();
@@ -42,8 +29,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseCors("AllowReactApp");
 
 // DI - UseApiServices
 app.UseApiServices();

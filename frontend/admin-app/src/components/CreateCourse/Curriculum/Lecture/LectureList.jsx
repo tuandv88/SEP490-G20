64e3a1d6dcb7020b4a749ade78584f7ab8d90540
TuanDv <vinhtuan9668@gmail.com>
2024-11-28 12:25:28 +0700

    Fix: Interface

--- frontend/admin-app/src/components/CreateCourse/Curriculum/Lecture/LectureList.jsx
+++ frontend/admin-app/src/components/CreateCourse/Curriculum/Lecture/LectureList.jsx
@@ -41,6 +41,7 @@ export default function LectureList({
           onVideoRemove={() => onVideoRemove(chapterIndex, lectureIndex)}
           setIsUpdateLecture={setIsUpdate}
           isUpdateLecture={isUpdate}
+          courseId={courseId}
         />
       ))}
     </>

0681345e25a403b94624ab440a6d9e6bd021d8f5
truongBui2002 <109296969+truongBui2002@users.noreply.github.com>
2024-12-01 00:37:37 +0700

    update

---
+++ backend/Services/Learning/Learning.API/Endpoints/Quizs/GetQuizAssessmentSubssmionEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Learning.Application.Models.Quizs.Dtos;
+using Learning.Application.Models.Quizs.Queries.GetQuizAssessment;
+using Learning.Application.Models.Quizs.Queries.GetQuizAssessmentSubmission;
+
+namespace Learning.API.Endpoints.Quizs;
+public record GetQuizAssessmentSubmissionResponse(List<QuizSubmissionAssessmentDto> Quízs);
+public class GetQuizAssessmentSubssmionEndpoint : ICarterModule {
+    public void AddRoutes(IEndpointRouteBuilder app) {
+
+        app.MapGet("/quizs/assessment/submission", async (ISender sender) => {
+
+            var result = await sender.Send(new GetQuizAssessmentSubmissionQuery());
+
+            var response = result.Adapt<GetQuizAssessmentSubmissionResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetQuizAssessmentSubmission")
+        .Produces<GetQuizAssessmentSubmissionResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Quiz Assessment Submission");
+    }
+}
---
+++ backend/Services/Learning/Learning.Application/Models/Quizs/Dtos/QuizSubmissionAssessmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Learning.Application.Models.Quizs.Dtos;
+public record QuizSubmissionAssessmentDto(
+    Guid QuizId,
+    string Title,
+    bool IsActive,
+    List<AssessmentDto> QuizSubmissions
+);
+public record AssessmentDto(
+  Guid QuizSubmissionId,
+  DateTime SubmissionDate
+);

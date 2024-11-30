8642b9a82cb687e03682a38469102dff84140606
truongBui2002 <109296969+truongBui2002@users.noreply.github.com>
2024-12-01 03:22:46 +0700

    update

--- frontend/admin-app/build_docker.sh
+++ frontend/admin-app/build_docker.sh
@@ -5,4 +5,5 @@ docker build \
     --build-arg VITE_BASE_URL_AUTH="https://accounts.icoder.vn" \
     --build-arg VITE_BASE_URL_CALLBACK="https://admin.icoder.vn" \
     --build-arg VITE_APP_TITLE="ICODER" \
+    --build-arg VITE_OAUTH_CLIENT_ID="admin.icoder.vn" \
     -t hub.icoder.vn/k8s/admin-frontend:v1.51 .

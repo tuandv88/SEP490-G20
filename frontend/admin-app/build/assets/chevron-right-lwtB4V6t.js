8454c4f4e71e0e622ece5b8176e1362d9a0b201e
LAMRICE <lamhllm24@gmail.com>
2024-12-02 10:32:58 +0700

    cd

--- frontend/admin-app/build/assets/chevron-right-lwtB4V6t.js
+++
@@ -1,7 +0,0 @@
-import{c as t}from"./index--0QK1py9.js";/**
- * @license lucide-react v0.456.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
-//# sourceMappingURL=chevron-right-lwtB4V6t.js.map

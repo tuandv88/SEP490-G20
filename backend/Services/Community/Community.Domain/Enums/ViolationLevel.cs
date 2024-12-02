69bdad31ddf56d3d4c076c4fb2951c987398d868
hamanhfu.me <hamanhfu.me@gmail.com>
2024-12-03 02:59:41 +0700

    add

--- backend/Services/Community/Community.Domain/Enums/ViolationLevel.cs
+++ backend/Services/Community/Community.Domain/Enums/ViolationLevel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Community.Domain.Enums;
+﻿namespace Community.Domain.Enums;
 
 // Bạn có thể tùy chỉnh ViolationLevel dưới dạng enum hoặc lớp riêng biệt
 public enum ViolationLevel

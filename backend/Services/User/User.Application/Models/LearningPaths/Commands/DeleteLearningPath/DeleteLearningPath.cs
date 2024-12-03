f436c5320cfd4d16fc2be68d5a8c7926cdc8bbb9
daidthe161326 <damtuandai2002@gmail.com>
2024-12-03 08:27:52 +0700

    update delete learningPath

--- backend/Services/User/User.Application/Models/LearningPaths/Commands/DeleteLearningPath/DeleteLearningPath.cs
+++ backend/Services/User/User.Application/Models/LearningPaths/Commands/DeleteLearningPath/DeleteLearningPath.cs
@@ -1,8 +1,10 @@
 ﻿using BuildingBlocks.CQRS;
+using Microsoft.AspNetCore.Authorization;
 using System;
 
 namespace User.Application.Models.LearningPaths.Commands.DeleteLearningPath
 {
+    [Authorize]
     // Định nghĩa truy vấn để xóa LearningPath và các PathStep có LearningPathId
     public record DeleteLearningPathQuery(Guid LearningPathId) : IQuery<bool>;
 }
